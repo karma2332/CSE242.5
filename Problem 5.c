6778032ab5b0ae77f117e71ff06d8d0744462c1d
karma2332 <rubaiyet12@gmail.com>
2025-11-11 22:10:44 +0600

    Create Problem 5.c

---
+++ Problem 5.c
@@ -0,0 +1,32 @@
+//Declare an Array, take Size, Elements dynamically. 
+//Then print Even indexed elements from the Array.
+
+#include <stdio.h>
+int main(){
+int n,m,even;
+printf("Enter the size of the array : ");
+scanf("%d",&n);
+
+printf("Enter how many number will be there 0<=%d: ",n);
+scanf("%d",&m);
+
+int myNum[n];
+printf("Enter the %d values ",m);
+for(int i = 0;i < m;i++){
+        scanf("%d",&myNum[i]);}
+
+
+printf("The %d values of the array are: ",m);
+for(int i = 0;i < m;i++){
+     printf("%d ",myNum[i]);}
+
+ printf("\nEven indexed elements from the array are: ");
+    for (int i = 0; i < m ; i++) {
+        if (i % 2 == 0) { 
+            printf("%d ", myNum[i]);
+        }
+
+    }
+
+return 0;
+}
