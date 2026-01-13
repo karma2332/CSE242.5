cf77d2407027ef73d90eb6ee210cf20f32b0032a
karma2332 <rubaiyet12@gmail.com>
2026-01-13 02:05:26 +0000

    Create linkedListinsertion.c

---
+++ linkedListinsertion.c
@@ -0,0 +1,58 @@
+// C Program: Insertion at Beginning
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+Node *createNode(int data) {
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// ADD-ON: Insert at Beginning
+Node *insertAtBeginning(Node *first, int data) {
+    Node *newNode = createNode(data);
+    newNode->next = first;
+    return newNode;
+}
+
+int main() {
+    Node *first = NULL;
+    Node *temp = NULL;
+    int n, value, i;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        printf("Enter value: ");
+        scanf("%d", &value);
+
+        if (first == NULL) {
+            first = createNode(value);
+            temp = first;
+        } else {
+            temp->next = createNode(value);
+            temp = temp->next;
+        }
+    }
+    int k;
+ printf("Enter insertion value: ");
+        scanf("%d", &k);
+    // Insertion at Beginning
+    first = insertAtBeginning(first, k);
+
+    printf("Linked List: ");
+    temp = first;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
